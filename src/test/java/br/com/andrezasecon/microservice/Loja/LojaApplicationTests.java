9fe429b240de0958738b969940d5afff5a0589a4
Andreza Secon <andrezasecon@gmail.com>
2021-11-18 12:15:35 -0300

    Create project Spring

---
+++ src/test/java/br/com/andrezasecon/microservice/Loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andrezasecon.microservice.Loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
